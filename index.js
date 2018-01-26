4a92cbd6f57ef1004439f51e024688789bbaf60e
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-01-26 15:03:13 +0100

    exercise 1

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const app = require('./src/app');
 const port = 3000;
 
 // start server here
+app.listen(3000);
 console.log(chalk.green('Hello web server'));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
-const app = '... please declare your Express web server here';
+var express = require('express');
+const app = express();
 
 module.exports = app;
