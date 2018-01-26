0952c3044e570c04d7211c82eb17364b96313f9e
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-01-26 16:21:16 +0100

    exercise 5

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ const port = 3000;
 
 //get method
 app.get('/bingo', function(req, res) {
+
+  var params = req.query.mynumbers;
+  var numbers;
   fs.readFile("src/numbers.txt", "utf8", function(err, data){
     if(err) throw err;
-    res.send(data.split().join());
-  });
+    numbers = data.split('\n').join().slice(0, -1);
+    if(params){
+      // j'ai pas fais gaffe à l'ordre des chiffres dans le parametre !
+      switch(params === numbers){
+          case true: res.send('Bingo');
+          break
+          case false: res.send('The bingo game is already started, sorry your numbers doesn\'t match with known numbers 31, 10, 80, 44, 66 ; so you can not say Bingo');
+          break
+        }
+    }else{
+    res.send(`The bingo game is already started and known numbers are ${numbers}`);
+  }
+});
 })
-
-
 // start server here
 app.listen(process.argv[2] || 3000);
 console.log(chalk.green('Hello web server'));
