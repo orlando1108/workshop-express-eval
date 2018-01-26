cf5c33730f8a69eb62b9b66d33462bd2deb5023e
orlando1108 <erwan.raulo2015@campus-eni.fr>
2018-01-26 15:17:03 +0100

    exercise 3

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = require('./src/app');
 // port
 const port = 3000;
 
+//get method
+app.get('/bingo', function(req, res) {
+     res.end('Bingo');
+   })
 // start server here
 app.listen(process.argv[2] || 3000);
 console.log(chalk.green('Hello web server'));
